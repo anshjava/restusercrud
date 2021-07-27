7c2ad371fc396d20690d7a01917300434e3c1f13
Andrey <ansh.msk@gmail.com>
2021-07-27 04:02:06 +0300

    test added

--- src/test/java/ru/kamuzta/restusercrud/RestusercrudApplicationTests.java
+++ src/test/java/ru/kamuzta/restusercrud/RestusercrudApplicationTests.java
@@ -1,13 +1,32 @@
 package ru.kamuzta.restusercrud;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import ru.kamuzta.restusercrud.model.User;
+import ru.kamuzta.restusercrud.service.UserService;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class RestusercrudApplicationTests {
 
+    @Autowired
+    UserService userService;
+
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void crudTest() {
+        User user = new User(0L,"TestFirstName","TestLastName", LocalDate.now());
+        User savedUser = userService.saveUser(user);
+        Assert.isTrue(user.getBirthdate().equals(savedUser.getBirthdate()), "Дни рождения не совпадают");
+        User loadedUser = userService.findById(savedUser.getId());
+        Assert.isTrue(savedUser.getBirthdate().equals(loadedUser.getBirthdate()), "Дни рождения не совпадают");
     }
 
 }
